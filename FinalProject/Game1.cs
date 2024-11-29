1c03d889013b8176631255b0b5034c292f5b3674
William <94484815+WinningWillow04@users.noreply.github.com>
2024-11-28 22:14:44 -0500

    I'm not sure what changed but I gotta commit

--- FinalProject/Game1.cs
+++ FinalProject/Game1.cs
@@ -247,9 +247,11 @@ namespace MonogameProject3_Spaceship
 			_spriteBatch.Draw(asteroidSprite1, new Vector2(ast1.position.X - Asteroid.radius, ast1.position.Y - Asteroid.radius), Color.White);
 			_spriteBatch.Draw(asteroidSprite2, new Vector2(ast2.position.X - Asteroid.radius, ast2.position.Y - Asteroid.radius), Color.White);
 			_spriteBatch.Draw(asteroidSprite3, new Vector2(ast3.position.X - Asteroid.radius, ast3.position.Y - Asteroid.radius), Color.White);
+            _spriteBatch.Draw(swordGreen1, new Vector2(ast3.position.X - Asteroid.radius, ast3.position.Y - Asteroid.radius), Color.White);
 
-			// Displaying Timer and Score
-			_spriteBatch.DrawString(timerFont, "Time: " + secondsElapsed, new Vector2(_graphics.PreferredBackBufferWidth / 2, 30), Color.White);
+
+            // Displaying Timer and Score
+            _spriteBatch.DrawString(timerFont, "Time: " + secondsElapsed, new Vector2(_graphics.PreferredBackBufferWidth / 2, 30), Color.White);
 			_spriteBatch.DrawString(scoreFont, "Score: " + controller.playerScore, new Vector2(_graphics.PreferredBackBufferWidth / 4, 30), Color.White);
 
             // Display game-over text if the game ends
